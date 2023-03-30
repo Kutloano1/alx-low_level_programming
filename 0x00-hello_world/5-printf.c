2d7dbbd6d9777ffafd6f63dbc4ec2c685f106db5
kutloano1 <olebogeng.molapisi1@gmail.com>
2023-03-30 11:52:55 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - starting point for program execution
+ *
+ * Return: 0 when succesful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
